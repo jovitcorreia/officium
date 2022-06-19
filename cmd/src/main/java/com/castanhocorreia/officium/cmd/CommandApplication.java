69b68fb85aa4acec3d3af100e2a8446257a0fb62
João Correia <me@castanhocorreia.com>
2022-06-19 05:44:55 -0300

    Generate Maven project

---
+++ cmd/src/main/java/com/castanhocorreia/officium/cmd/CommandApplication.java
@@ -0,0 +1,11 @@
+package com.castanhocorreia.officium.cmd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(CommandApplication.class, args);
+  }
+}
